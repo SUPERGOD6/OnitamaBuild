ad68e8858f89e18e89d0f13eb5e8d9430350e333
Dylan Warwick <s216298792@mandela.ac.za>
2018-10-10 23:21:00 +0200

    UI nearly done

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour {
+
+  public void Magic()
+  {
+    Text t = GameObject.FindGameObjectWithTag("test").GetComponent<Text>();
+    t.text = "Hello World";
+  }
+  public void NewGame()
+  {
+    SceneManager.LoadScene("GameScene",LoadSceneMode.Single);
+  }
+  public void ExitGame()
+  {
+    SceneManager.LoadScene("ExitMenu",LoadSceneMode.Additive);
+  }
+  public void StartGame()
+  {
+    //SceneManager.LoadScene("GameScene",LoadSceneMode.Single);
+    SceneManager.UnloadSceneAsync("ExitMenu");
+  }
+  public void StopGame()
+  {
+    SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+  }
+  public void RuleBook()
+  {
+    SceneManager.LoadScene("RuleBook",LoadSceneMode.Single);
+  } 
+	public void BackClick()
+  {
+    SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+  }
+  public void QuitGame()
+  {
+    #if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+    #else
+      Application.Quit();
+    #endif
+  }
+}
